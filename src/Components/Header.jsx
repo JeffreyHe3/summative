f427262151f64770b2e493956dec8615ce2141e0
JeffreyHe3 <hejeffrey7@gmail.com>
2025-05-22 21:02:23 -0400

    NO MORE USECALLBACK

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,45 @@
 import "./style.css"
-import { useCallback } from "react"
 import { useNavigate } from "react-router-dom"
 import { useStoreContext } from "../Context";
 
 function Header() {
+  const navigate = useNavigate();
+  const { fName, email, setEmail } = useStoreContext();
+
+  const debounce = (func, delay) => {
+    let timer;
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    }
+  }
+
+  const onSearch = debounce((e) => {
+    if (e.target.value) {
+      navigate(`/movies/search/${e.target.value}`);
+    }
+  }, 500);
 
   return (
-    <div></div>
+    <div id="header">
+      <h1 className="title">Jeffrey's Movies</h1>
+      {email ? (
+        <>
+          <h1 className="title">{`Hi ${fName}!`}</h1>
+          <button className="headerButtons" onClick={() => navigate("/cart")}>Cart</button>
+          <button className="headerButtons" onClick={() => navigate("/settings")}>Settings</button>
+          <button className="headerButtons" onClick={() => { setEmail(null); navigate("/"); }}>Logout</button><br />
+          <input type="text" id="searchBar" placeholder="Search Movies Here" onInput={(e) => onSearch(e)} />
+        </>
+      ) : (
+        <>
+          <button className="headerButtons" onClick={() => navigate("/login")}>Login</button>
+          <button className="headerButtons" onClick={() => navigate("/register")}>Register</button>
+        </>
+      )}
+    </div>
   );
 }
 
